1af365034cfb88c184767644723fc644638eb5d3
ritik0912kumar <126445623+ritik0912kumar@users.noreply.github.com>
2024-11-14 14:20:47 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+class TrafficSignal:
+    def __init__(self, red, yellow, green, minimum, maximum):
+        self.red = red
+        self.yellow = yellow
+        self.green = green
+        self.minimum = minimum
+        self.maximum = maximum
+        self.signalText = "30"
+        self.totalGreenTime = 0
